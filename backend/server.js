e0459abda6c75d3267c1ac0b8430ec7b3607533e
TajammulKhan <tajammul.khan@tibilsolutions.com>
2024-10-08 17:31:56 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -76,6 +76,6 @@ app.post('/convert', upload.single('file'), (req, res) => {
 });
 
 // Start the server on port 5000
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
 });
